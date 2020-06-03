31f631890e98048f1174d5c70dc14a4700d91ed8
Ryota Sakamoto <sakamo.ryota+github@gmail.com>
2020-06-03 09:43:10 +0900

    init

---
+++ src/main/java/me/hxa/dev/sample/apmsampleserver/ApmSampleServerApplication.java
@@ -0,0 +1,13 @@
+package me.hxa.dev.sample.apmsampleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmSampleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApmSampleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hxa/dev/sample/apmsampleserver/ApmSampleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hxa.dev.sample.apmsampleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApmSampleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
